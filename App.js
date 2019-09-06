715d703e850c91ecbb4ab6296de0cede0f2b6759
Dave March <dbmarch@gmail.com>
2019-09-05 21:22:47 -0400

    add a modal for entry

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet,View , FlatList} from 'react-native';
+import { StyleSheet, View, FlatList, Button} from 'react-native';
 import {isEmpty} from 'lodash'
 import Item from './components/Item'
 import ItemInput from './components/ItemInput'
@@ -7,12 +7,14 @@ import ItemInput from './components/ItemInput'
 // the key extractor below is only required if your data doesn't have the correct shape.
 export default function App() {
   const [list, setList] = useState([])
+  const [isAddMode, setIsAddMode] = useState(false)
 
   const addItemHandler = (item)=> {
     const key = Math.random().toString()
     if (!isEmpty(item) ){
       setList([...list, {key, value: item}])
     }
+    setIsAddMode(false);
   }
 
   const removeItemHandler= key => {
@@ -22,7 +24,8 @@ export default function App() {
 
   return (
     <View style = {styles.screen}>
-      <ItemInput placeholder="Shopping List" addItemHandler={addItemHandler}  ></ItemInput>
+      <Button title="Add Item" onPress={()=>setIsAddMode(true)}/>
+      <ItemInput placeholder="Shopping List" addItemHandler={addItemHandler} visible={isAddMode} ></ItemInput>
        <FlatList 
           data = {list} 
           renderItem= {itemData=>(
--- components/ItemInput.js
+++ components/ItemInput.js
@@ -1,34 +1,42 @@
 import React, {useState} from 'react'
-import {View, TextInput, Button, StyleSheet} from 'react-native'
+import {View, TextInput, Button, StyleSheet, Modal} from 'react-native'
+import { AuthSession } from 'expo';
 
-const ItemInput = ( {placeholder,  addItemHandler }) => {
+const ItemInput = ( {placeholder,  addItemHandler, visible }) => {
    const [item, setItem]= useState('')
 
    const itemInputHandler = (enteredText) => setItem(enteredText)
 
    return (
-   <View style = {styles.inputContainer}>
-   <TextInput 
-     placeholder = {placeholder}
-     onChangeText = { itemInputHandler}
-     style = {styles.textInput}
-     value = {item}
-     />
-   <Button title = "ADD" onPress={()=>{
-      addItemHandler(item)
-      setItem('')} } />
-  </View>
+    <Modal visible = {visible} animationType = "slide">
+      <View style = {styles.inputContainer}>
+         <TextInput 
+           placeholder = {placeholder}
+           onChangeText = { itemInputHandler}
+           style = {styles.textInput}
+           value = {item}
+           />
+         <Button title = "ADD" onPress={()=>{
+            addItemHandler(item)
+            setItem('')} } />
+       </View>
+    </Modal>
    )
 }
 
 
 const styles = StyleSheet.create({
+   modal: {
+      margin: 'auto'
+   },
    inputContainer: {
-      flexDirection: 'row', 
-      justifyContent: 'space-between',
+      flex: 1,
+      flexDirection: 'column', 
+      justifyContent: 'center',
        alignItems: 'center' 
       },
     textInput: {
+     marginBottom: 10,
      borderBottomColor: 'black', 
      borderBottomWidth: 1, 
      padding: 10, 
